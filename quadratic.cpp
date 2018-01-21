ddda8c4265f05f7c35924f8063e9d3c7dc664a8f
Faria Begum Riya <riya09@users.noreply.github.com>
2018-01-22 00:03:17 +0600

    Update quadratic.cpp

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,19 +1,27 @@
+/*
+Given coefficients of a quadratic equation,find the solution.
+*/
 #include <bits/stdc++.h>
 using namespace std;
-float solve(float a,float b,float c){
+void solve(float a,float b,float c){
     float discrm= (b*b)-4*a*c;
     float x1,x2;
     if (discrm>=0){
         x1=(-b+sqrt(discrm))/(2*a);
         x2=(-b-sqrt(discrm))/(2*a);
+        cout<<x1<<","<<x2<<'\n';
     }
     else{
-        float real_part=-b/(2*a);
-        float img=sqrt(-discrm)/(2*a);
-        
+        x1=-b/(2*a);
+        x2=sqrt(-discrm)/(2*a);
+        cout<<x1<<"+"<<x2<<"i"<<'\n';
+        cout<<x1<<"-"<<x2<<"i"<<'\n';
+
     }
 }
 int main(){
-
+    float a,b,c;
+    cin>>a>>b>>c;
+    solve(a,b,c);
     return 0;
 }
