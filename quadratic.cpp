ea130f6f6e607172a1df3ab7397ea47a4ddcdf89
Faria Begum Riya <riya09@users.noreply.github.com>
2018-01-21 23:58:41 +0600

    Update quadratic.cpp

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+float solve(float a,float b,float c){
+    float discrm= (b*b)-4*a*c;
+    float x1,x2;
+    if (discrm>=0){
+        x1=(-b+sqrt(discrm))/(2*a);
+        x2=(-b-sqrt(discrm))/(2*a);
+    }
+    else{
+        float real_part=-b/(2*a);
+        float img=sqrt(-discrm)/(2*a);
+        
+    }
+}
 int main(){
-    
+
     return 0;
 }
