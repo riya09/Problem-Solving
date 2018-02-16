f53206c50405b799fb9fc3ef58d3da619f200e13
Faria Begum Riya <riya09@users.noreply.github.com>
2018-02-16 23:43:41 +0600

    Update zero.cpp

--- zero.cpp
+++ zero.cpp
@@ -1,3 +1,6 @@
+/*Write an algorithm such that if an element in an MxN
+ matrix is 0, its entire row and column is set to 0.*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
@@ -17,8 +20,9 @@ int main(){
             if(row[i]==1 ||col[j]==1){
                 arr[i][j]=0;
             }
-            cout<<arr[i][j]<<'\n';
+            cout<<arr[i][j]<<" ";
         }
+        cout<<'\n';
     }
     return 0;
 }
